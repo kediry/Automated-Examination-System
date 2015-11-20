2953a5b0617a285bea6e0d98898356f6d577a0f0
NANI-PC <nani-pc@ubuntu.ubuntu-domain>
2015-11-20 23:52:03 +0530

    Fixed some bugs in installation.

--- Install/index.php
+++ Install/index.php
@@ -1,87 +1,40 @@
 <html>
-	<link rel="stylesheet" type="text/css" href="../includes/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="../includes/css/theme.css" />
+<link rel="stylesheet" type="text/css" href="../includes/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="../includes/css/theme.css" />
 <body>
 	<div class="panel panel-default col-md-8 col-md-offset-2">
-		<div class="panel-heading"><img src="../includes/img/rgu.png" class="img-responsive img-rounded pull-right" alt="Responsive image" height="50" width="50"><h3 class="text-info">Welcome to AOES Installation </h3></div>
+		<div class="panel-heading"><h3 class="text-info">Welcome to AES Installation </h3></div>
 		<div class="panel-body">
-			<p><i class="glyphicon glyphicon-leaf"></i> This Project is Developed as a B.Tech Final Year Project of GrandMasterS (K.Gowri Sankara Naidu[Nani], G.Mahesh, M.Siva Kumar) Team Under the Guidence of <strong>SHRIKANT MALVIYA</strong> <span class='small'>(M.Tech, Lecturer)</span> in Computer Science Department(C.S.E) of R.K.Valley, RGUKT.</p>
-			<p><i class="glyphicon glyphicon-leaf"></i> Automated Online Examination System is a Web Based Application. The main advantage of this system is that by using this Application anybody can conduct the exam without coding knowledge.</p><br>
-			<h3 class="text-info">AOES Team </h3>
-				<div class="well well-sm col-md-6">
-						<div class="media">
-							<a class="thumbnail pull-left" href="#">
-								<img class="media-object" src="../includes/img/srikant.jpg" width="80" height="80">
-							</a>
-							<div class="media-body">
-								<h4 class="media-heading">SHRIKANT MALVIYA</h4>
-								<p><span class="small">M.Tech, Lecturer in RGUKT (Project Guide)</span></p>
-								<p>
-									<a href="mailto:shrikant.iet6153@gmail.com" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-envelope" style="color:white;"></i> shrikant.iet6153@gmail.com</a>
-									<a href="#" class="btn btn-xs btn-success"><span class="glyphicon glyphicon-phone" style="color:white;"></span> 94518519025</a>
-								</p>
-							</div>
+			<div class='col-md-12'>
+				<div class="well well-sm col-md-6 col-md-offset-3">
+					<div class="media">
+						<a class="thumbnail pull-left" href="#">
+							<img class="media-object" src="../includes/img/nani.jpg" width="80" height="80">
+						</a>
+						<div class="media-body">
+							<h4 class="media-heading">K Gowri Sankara Naidu (Nani)</h4>
+							<p>Developer</p>
+							<p>
+								<a href="mailto:k.g.s.naidu2237@gmail.com" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-envelope" style="color:white;"></i> k.g.s.naidu2237@gmail.com</a>
+							</p>
 						</div>
-					   </div>
-				<div class="well well-sm col-md-6">
-						<div class="media">
-							<a class="thumbnail pull-left" href="#">
-								<img class="media-object" src="../includes/img/nani.png" width="80" height="80">
-							</a>
-							<div class="media-body">
-								<h4 class="media-heading">K Gowri Sankara Naidu (Nani)</h4>
-								<p><span class="small"> [R082237] (Team Member)</span></p>
-								<p>
-									<a href="mailto:k.g.s.naidu2237@gmail.com" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-envelope" style="color:white;"></i> k.g.s.naidu2237@gmail.com</a>
-									<a href="#" class="btn btn-xs btn-success"><span class="glyphicon glyphicon-phone" style="color:white;"></span> 9440988454</a>
-								</p>
-							</div>
-						</div>
-					   </div>
-					   <div class="well well-sm col-md-6">
-						<div class="media">
-							<a class="thumbnail pull-left" href="#">
-								<img class="media-object" src="../includes/img/mahi.jpg" width="80" height="80">
-							</a>
-							<div class="media-body">
-								<h4 class="media-heading">G Mahesh</h4>
-								<p><span class="small"> [R082307] (Team Member)</span></p>
-								<p>
-									<a href="mailto:mahifromcse@gmail.com" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-envelope" style="color:white;"></i> mahifromcse@gmail.com</a>
-									<a href="#" class="btn btn-xs btn-success"><span class="glyphicon glyphicon-phone" style="color:white;"></span> 9491354265</a>
-								</p>
-							</div>
-						</div>
-					   </div>
-					   <div class="well well-sm col-md-6">
-						<div class="media">
-							<a class="thumbnail pull-left" href="#">
-								<img class="media-object" src="../includes/img/agastin.jpg" width="80" height="80">
-							</a>
-							<div class="media-body">
-								<h4 class="media-heading">M Siva Kumar</h4>
-								<p><span class="small"> [R082222] (Team Member)</span></p>
-								<p>
-									<a href="mailto:agastin4u@gmail.com" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-envelope" style="color:white;"></i> agastin4u@gmail.com</a>
-									<a href="#" class="btn btn-xs btn-success"><span class="glyphicon glyphicon-phone" style="color:white;"></span> 8331869981</a>
-								</p>
-							</div>
-						</div>
-					   </div>
-					   <h3 class="text-danger">Terms & Conditions:</h3>
-					   <p class=''><i class='glyphicon glyphicon-fire'></i>&emsp;<strong><img src="../includes/img/chrome.png" width="20" height="20"> Google Chrome Latest Version</strong> is Compatable for this application. We recommend to use Google Chrome Latest Version for Better View and Processing.</p>
-					   <p class='text-danger'><i class='glyphicon glyphicon-fire'></i>&emsp;In Next Page Carefully Enter <strong> MySql Database Configurations</strong>. If you don't know anything about DB host, DB username, DB Password Contact the Person who knows about those. Without knowing those Don't install. If you do so then software will not work. </p>
-					   <p class=''><i class='glyphicon glyphicon-fire'></i>&emsp;Always Remember <strong>coordinator@aoes.com</strong> was the login email id for Examination Coordinator. Email won't change <strong>Password was what you enter in the next page</strong>. Don't forget Password and User name of Coordinator Because They won't recover.</p>
-					   <div align=center class='col-xs-6 col-xs-offset-3'>
-						<form action="start.php" method='post'>
-							<input type='checkbox' name='accept' value='yes' required > I accept Terms&Conditions.
-							<br><br>
-							<input type="submit" class="btn btn-primary" value="Start Installation">
-					   </form>
-					   </div>
+					</div>
+				</div>
+			</div>
+			<h3 class="text-danger">Terms & Conditions:</h3>
+			<p class=''><i class='glyphicon glyphicon-fire'></i>&emsp;<strong><img src="../includes/img/chrome.png" width="20" height="20"> Google Chrome Latest Version</strong> is Compatable for this application. We recommend to use Google Chrome Latest Version for Better View and Processing.</p>
+			<p class='text-danger'><i class='glyphicon glyphicon-fire'></i>&emsp;In Next Page Carefully Enter <strong> MySql Database Configurations</strong>. If you don't know anything about DB host, DB username, DB Password Contact the Person who knows about those. Without knowing those Don't install. If you do so then software will not work. </p>
+			<p class=''><i class='glyphicon glyphicon-fire'></i>&emsp;Always Remember <strong>coordinator@aoes.com</strong> was the login email id for Examination Coordinator. Email won't change <strong>Password was what you enter in the next page</strong>. Don't forget Password and User name of Coordinator Because They won't recover.</p>
+			<div align=center class='col-xs-6 col-xs-offset-3'>
+				<form action="start.php" method='post'>
+					<input type='checkbox' name='accept' value='yes' required > I accept Terms&Conditions.
+					<br><br>
+					<input type="submit" class="btn btn-primary" value="Start Installation">
+				</form>
+			</div>
 		</div>
 		<hr>
-		<div class="small" align="center">&copy; Computer Science Department<br>RGUKT, APIIIT<br>R.K.Valley-516329.</div>
+		<div class="small" align="center">Automated Examination System</div>
 		<br>
 	</div>
 </body>
